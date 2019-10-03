fc4449578c0a1cebac1b5e2064c89d5719ea82f6
NNboru <52328161+NNboru@users.noreply.github.com>
2019-10-04 00:44:02 +0530

    Update sieve-minDiv.cpp

--- sieve-minDiv.cpp
+++ sieve-minDiv.cpp
@@ -6,44 +6,102 @@
 #define pii pair<int,int>
 #define F first
 #define S second
-#define fr(i,n) for(int i=0;i<n;i++)
+#define fr(i,n) for(int i=0;i<n;++i)
 #define pr cout<<
-#define en <<endl
+#define en <<'\n'
 #define sp <<' '
 using namespace std;
 typedef long long ll;
 
 // int ar[200001];
 
-int mind[2000000]; // mind[i] = min prime divisor of i.
+int mind[100001],prm[100001]; // mind[i] = min prime divisor of i.
+int pcnt=0; // count no of primes in prm.
 
-vector<int> prm;
+// making sieve.
 void build_sieve(int n) {
-	mind[0] = mind[1] = 1;
-	for (int i = 2; i <= n; ++i) {
-		if (mind[i] == 0) {
-			prm.push_back(i);
-			mind[i] = i;
+	int mi,pd,j;
+	mind[1]=1;
+	for (int i = 2; i <= n; ++i)
+	{
+		if (mind[i] == 0)
+		{
+			prm[pcnt++]=i;
+			mind[i]=i;
+			mi = i;
 		}
-		for (int j = 0; j < int(prm.size()) && prm[j] <= mind[i] && i * prm[j] <= n; ++j) {
-			mind[i * prm[j]] = prm[j];
+		else mi=mind[i];
+		j=0;
+		while(j<pcnt && prm[j]<=mi)
+		{
+			if( (pd=i*prm[j]) >n) break;
+			mind[pd] = prm[j];
+			++j;
 		}
 	}
 }
 
+// making factors of N using sieve.
+int divs[100][100],sz[100],cnt;
+void facs(int ind,int prod)
+{
+	// needs divs, cnt.
+	if(ind==cnt)
+	{
+		ll d,tmp;
+		for(int i=0;i<sz[ind];++i)
+		{
+			d=prod*divs[ind][i];
+			// d is the factor generated.
+			pr d en;
+		}
+	}
+	else
+	{
+		for(int i=0;i<sz[ind];++i)
+			facs(ind+1,prod*divs[ind][i]);
+	}
+}
+int factors(int n)
+{
+	// max no of prime factors = 10.
+	for(int i=0;i<10;++i)
+	{
+		sz[i]=1;
+		divs[i][0]=1;
+	}
+	cnt=0;
+	// adding factors of n.
+	int num=n ,old=mind[num];
+	while(mind[num]!=1) // making divs
+	{
+		if(mind[num]==old)
+		{
+			divs[cnt][sz[cnt]]=divs[cnt][sz[cnt]-1]*old;
+			sz[cnt]++;
+			num/=old;
+		}
+		else
+		{
+			cnt++;
+			old=mind[num];
+		}
+	}
+	facs(0,1); // generating factors.
+}
 
 main()
 {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int t,n ;
-	build_sieve(200000);
-	pr "Primes: ";fr(i,100) pr i<<" : "<<mind[i] en;
-	cin>>t;
-	while(t--)
-	{
-		cin>>n;
-		
-	}
+	int t,N;
+	N=60 ;
+	build_sieve(100000);
+	pr "No of primes: "<<pcnt en;
+	// pr "least prime factor: ";fr(i,100) pr i<<" : "<<mind[i] en;
+	
+	factors(N);
+	
+	
 }
 
 
